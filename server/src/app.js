25eb50942e87fc7ae1d084d62230fef249983f21
eiffeltower96 <utkarsh024385@gmail.com>
2026-06-16 22:24:56 +0530

    prepare frontend deployment

--- server/src/app.js
+++ server/src/app.js
@@ -32,8 +32,13 @@ app.get("/api/profile", protect, (req, res) => {
     });
 
 });
-const PORT = 5000;
+const PORT =
+    process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
 });
